25138ca440f0d9bc50a94ec7584d1f20f10e218a
John Mark <dr3adlokk@gmail.com>
2018-06-08 12:51:03 -0700

    backstretch-yes-character-img-no

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+	$.backstretch([
+          "../images/background.jpg",
+         "../images/soul-eater.jpg",
+         "../images/soul-eater2.jpg",
+         "../images/soul-eater3.jpg",
+        ], {
+            duration: 10000,
+            fade: 900
+        });
+
+  console.log('here');
+});
