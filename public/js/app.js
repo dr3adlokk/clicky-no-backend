d6ad059f31b3051abb45c097751f75a3a1184f18
John Mark <dr3adlokk@gmail.com>
2018-06-08 16:27:59 -0700

    img-get-404

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
          "../images/soul-eater.jpg",
          "../images/soul-eater2.jpg",
          "../images/soul-eater3.jpg",
+        "../images/soul-eater4.jpg",
+        "../images/soul-eater5.jpg",
         ], {
             duration: 10000,
             fade: 900
